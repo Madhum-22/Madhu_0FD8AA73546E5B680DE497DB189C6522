e8b89b4842e70c05cac3566515ca4e5d23ff843a
Madhum-22 <144684177+Madhum-22@users.noreply.github.com>
2023-09-13 15:31:06 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,12 @@
+num = int(input("please enter the number you wish: "))
+if (num % 4==0):
+  if (num % 100==0):
+    if (num % 400==0): 
+  print("%d is a leap loop"% num)
+  print("%d is a leap loop" % num)
+else:
+  print("%d is not" % num)
+else:
+  print("%d is a leap loop year" % num) 
+else:
+  print("%d is not" % num)
